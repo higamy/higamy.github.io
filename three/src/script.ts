b45ae6ff8bdf41d450b4f72a1bae3fe4a41f2fc4
higamy <wshigham@gmail.com>
2021-07-05 21:02:15 +0100

    Updated models

--- three/src/script.ts
+++ three/src/script.ts
@@ -4,10 +4,10 @@
 // npx webpack --config ./three/src/webpack.config.js
 
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { GUI } from 'three/examples/jsm/libs/dat.gui.module'
 import { TWEEN } from 'three/examples/jsm/libs/tween.module.min'
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xcdf2f7);
@@ -24,14 +24,20 @@ camera.position.x = 3;
 camera.position.y = 6;
 camera.position.z = 6;
 
+let controls_active: boolean = true
+
+const projectNameTitle: HTMLElement = document.getElementById('projectNameTitle');
+const projectDescriptionContainer: HTMLElement = document.getElementById('projectDescriptionContainer');
+const closeDescriptionButton: HTMLElement = document.getElementById('closeDescriptionButton');
+
+closeDescriptionButton.addEventListener('click', () => {
+    currently_selected_exhibit.deselect();
+})
+
 const camera_start_position = camera.position;
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true
-controls.target = new THREE.Vector3(0, 1, 0)
-
 var axesHelper = new THREE.AxesHelper(5)
-scene.add(axesHelper)
+//scene.add(axesHelper)
 
 const light = new THREE.DirectionalLight(0xFFFFE0, 0.5);
 light.castShadow = true;
@@ -44,13 +50,14 @@ light.position.set(10, 10, 10);
 scene.add(light);
 
 const lightHelper = new THREE.DirectionalLightHelper(light)
-scene.add(lightHelper)
+//scene.add(lightHelper)
 
 const ambient_light = new THREE.AmbientLight(0x404040); // soft white light
 //scene.add(ambient_light);
 
-const containerMeshes = new Array()
+const containerMeshes = new Array
 
+const controls = new OrbitControls(camera, renderer.domElement);
 /* Floor */
 function add_floor() {
     var geo = new THREE.PlaneGeometry(3000, 3000);
@@ -61,12 +68,16 @@ function add_floor() {
     plane.rotateX(- Math.PI / 2);
     plane.receiveShadow = true
     scene.add(plane);
-}
-add_floor();
 
-const SCENE_SCALE_FACTOR = 1
+
+}
+//add_floor();
+
+camera.lookAt(new THREE.Vector3())
+
 const spot_light_helpers = new Array()
 let exhibits = new Array()
+let currently_selected_exhibit: Exhibit;
 const MAX_SPOTLIGHT_INTENSITY = 0.5;
 const LIGHT_INTENSITY_CHANGE_TIME = 250;
 
@@ -75,10 +86,14 @@ class Exhibit {
     container: THREE.Object3D
     activated: boolean
     current_tween: TWEEN.tween
+    name: string
+    startTarget: THREE.Vector3;
+    startPosition: THREE.Vector3;
 
-    constructor(light: THREE.Light, container: THREE.Object3D) {
+    constructor(light: THREE.Light, container: THREE.Object3D, projectName: string) {
         this.light = light;
         this.container = container;
+        this.name = projectName;
     }
 
     activate() {
@@ -103,16 +118,48 @@ class Exhibit {
     }
 
     select() {
-        console.log(this)
-        camera.position.set(this.container.position.x * SCENE_SCALE_FACTOR, this.container.position.y * SCENE_SCALE_FACTOR, this.container.position.z * SCENE_SCALE_FACTOR);
+        currently_selected_exhibit = this;
+
+        // Backup original view settings
+        this.startTarget = controls.target;
+        this.startPosition = camera.position.clone();
+
+        let endPosition = new THREE.Vector3(this.container.position.x - 2, this.container.position.y + 1, this.container.position.z + 3)
+        new TWEEN.Tween(camera.position)
+            .to({ x: endPosition.x, y: endPosition.y, z: endPosition.z }, 1000)
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .start();
+
+
+        // Tween
+        new TWEEN.Tween(controls.target)
+            .to({ x: endPosition.x, y: endPosition.y, z: endPosition.z - 1 }, 1000)
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .start();
+
+
+        projectNameTitle.innerHTML = this.name;
+        projectDescriptionContainer.classList.remove('hidden');
+    }
+
+    deselect() {
+        projectDescriptionContainer.classList.add('hidden');
+
+        new TWEEN.Tween(camera.position)
+            .to(this.startPosition, 1000)
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .start();
+
+        new TWEEN.Tween(controls.target)
+            .to(this.startTarget, 1000)
+            .easing(TWEEN.Easing.Quadratic.Out)
+            .start();
     }
 }
 
 function add_house() {
     new GLTFLoader().load('https://higamy.github.io/models/scene.glb',
         (gltf) => {
-
-            gltf.scene.scale.set(SCENE_SCALE_FACTOR, SCENE_SCALE_FACTOR, SCENE_SCALE_FACTOR);
             scene.add(gltf.scene);
 
             gltf.scene.traverse(function (node) {
@@ -129,16 +176,18 @@ function add_house() {
 
                     // Add a light
                     let light = new THREE.SpotLight(0xFFFF99, 0, 0, Math.PI / 12, 0.5);
-                    light.position.set(node.position.x * SCENE_SCALE_FACTOR, 10 + node.position.y * SCENE_SCALE_FACTOR, node.position.z * SCENE_SCALE_FACTOR)
+                    light.position.set(node.position.x, 10 + node.position.y, node.position.z)
                     light.target = node;
                     light.castShadow = true;
                     scene.add(light)
 
                     const lightHelper = new THREE.SpotLightHelper(light)
-                    scene.add(lightHelper)
+                    //scene.add(lightHelper)
+
+                    const projectName: string = node.name.replace('Container', '')
 
                     // Store a reference to the spot light
-                    const exhibit = new Exhibit(light, node)
+                    const exhibit = new Exhibit(light, node, projectName)
                     node.userData.exhibit = exhibit;
 
                     exhibits.push(exhibit);
@@ -187,7 +236,13 @@ function getMouseTarget(event: MouseEvent) {
     return raycaster.intersectObjects(containerMeshes, false);
 }
 
-renderer.domElement.addEventListener('mousemove', onMouseMove, false);
+
+if (window.PointerEvent) {
+    renderer.domElement.addEventListener('pointermove', onMouseMove, false);
+} else {
+    renderer.domElement.addEventListener('mousemove', onMouseMove, false);
+}
+
 const raycaster = new THREE.Raycaster();
 function onMouseMove(event: MouseEvent) {
     const intersects = getMouseTarget(event)
@@ -206,12 +261,14 @@ function onMouseMove(event: MouseEvent) {
 }
 
 // CLICK
-
-renderer.domElement.addEventListener('click', onClick, false);
+if (window.PointerEvent) {
+    renderer.domElement.addEventListener('pointerdown', onClick, false);
+} else {
+    renderer.domElement.addEventListener('mousedown', onClick, false);
+}
 
 function onClick(event: MouseEvent) {
-    const intersects = getMouseTarget(event)
-
+    const intersects = getMouseTarget(event);
 
     if (intersects.length > 0) {
         let exhibit = <Exhibit>intersects[0].object.userData.exhibit
@@ -223,14 +280,9 @@ const clock: THREE.Clock = new THREE.Clock()
 
 function animate() {
 
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-
     // Physics
     let delta = clock.getDelta()
     if (modelReady) mixer.update(delta);
-
-    controls.update(); // This must be called or the damping will not work
 
     //player.position.set(cubeBody.position.x, cubeBody.position.y, cubeBody.position.z);
     //player.quaternion.set(cubeBody.quaternion.x, cubeBody.quaternion.y, cubeBody.quaternion.z, cubeBody.quaternion.w);
@@ -242,6 +294,12 @@ function animate() {
     // for (let exhibit of exhibits) {
     //    exhibit.update_intensity(delta)
     // }
+
+    controls.update();
+
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+
 }
 animate();
 
