62735ef7477b8b179389922d5980604fd92a65b5
higamy <wshigham@gmail.com>
2022-02-27 18:29:44 +0000

    testing TW

--- TW/script.js
+++ TW/script.js
@@ -1,3 +1,5 @@
 var jqxhr = $.get("http://www.twstats.co.uk/uk60/index.php?page=player&id=920079&tab=history", function () {
     alert("success");
 })
+
+$('.vis').find('tr.row_a, tr.row_b').find('td:nth-of-type(1)').each((i, el) => console.log($(el).text().trim()))
