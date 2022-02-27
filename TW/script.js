58a2574d48ebf3152f068c0ad30c018036bcb51b
higamy <wshigham@gmail.com>
2022-02-27 18:45:34 +0000

    Added player IDs

--- TW/script.js
+++ TW/script.js
@@ -1,5 +1,14 @@
-var jqxhr = $.get("http://www.twstats.co.uk/uk60/index.php?page=player&id=920079&tab=history", function () {
-    alert("success");
-})
+$('.vis').find('tr.row_a, tr.row_b').find('td:nth-of-type(1)').each((i, el) => {
+    let player_name = $(el).text().trim();
+    console.log(player_name);
 
-$('.vis').find('tr.row_a, tr.row_b').find('td:nth-of-type(1)').each((i, el) => console.log($(el).text().trim()))
+    let player_id = $(el).find('a').attr('href').split('&id=')[1];
+    console.log(player_id);
+
+
+    var jqxhr = $.get(`https://www.twstats.co.uk/uk60/index.php?page=player&id=${player_id}&tab=history`, function (data) {
+        console.log(data)
+    })
+
+}
+)
