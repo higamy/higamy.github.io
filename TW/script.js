0cceb7696f95d4187dfa14272f4f627d532f0b2b
Will Higham <wshigham@gmail.com>
2022-03-07 09:38:52 +0000

    Updated TW data

--- TW/script.js
+++ TW/script.js
@@ -5,8 +5,9 @@ axios.get(`${githubLocation}/Config/tribes.json`)
     .then(data => {
         data = data.data
         console.log(data)
-        for (let serverConfig of data) {
+        for (let serverConfig of data.config) {
             const worldSelector = document.createElement('div');
-
+            worldSelector.innerHTML = serverConfig['server']
+            serverContainer.appendChild(worldSelector);
         }
     })
