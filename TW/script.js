26c69af8dbe9f89880188c4729bb6b8d9cf5fce8
Will Higham <wshigham@gmail.com>
2022-03-13 22:04:03 +0000

    Updated TW data

--- TW/script.js
+++ TW/script.js
@@ -8,28 +8,154 @@ Add tribe tag
 */
 
 
-const githubLocation = 'https://higamy.github.io/TW/Data'
+// Constants
+const githubLocation = 'https://higamy.github.io/TW/Data';
+const availableStatistics = ['points', 'ranks', 'villages', 'OD', 'ODA', 'ODD'];
+
+// DOM Elements
+const statisticSelector = document.getElementById("statisticSelector");
+const statisticsDropdownOptions = document.getElementById("statisticsDropdownOptions");
+const serverSelector = document.getElementById("serverSelector");
+const serverSelectorDropdownOptions = document.getElementById("serverSelectorDropdownOptions");
+const worldSelector = document.getElementById("worldSelector");
+const worldSelectorDropdownOptions = document.getElementById("worldSelectorDropdownOptions");
+
 const serverContainerEl = document.getElementById("serverContainer");
 const worldContainerEl = document.getElementById("worldContainer");
 const graphAdder = document.getElementById("graphAdder");
 const tribePopupContainer = document.getElementById("tribePopupContainer");
 const plotContainer = document.getElementById("plotContainer");
 
+let serverSelectors = [];
+let activeTribeSelector;
+let selectedMetric = availableStatistics[0];
 
-let serverSelectors = []
+
 
 axios.get(`${githubLocation}/Config/tribes.json`)
     .then(data => {
         data = data.data
         console.log(data)
+
         for (let serverConfig of data.config) {
-            const serverContainer = new ServerContainer(serverConfig);
-            serverSelectors.push(serverContainer);
+            serverSelectors.push(new ServerSelector(serverConfig));
         }
         serverSelectors[0].activate();
-        serverSelectors[0].worldButtons[0].activate()
+
     })
 
+class ServerSelector {
+    serverConfig
+    worldDropDowns
+    server
+
+    constructor(serverConfig) {
+        this.serverConfig = serverConfig;
+        this.server = serverConfig['server'];
+
+        const dropDownOption = new DropDownOption(serverSelector, serverSelectorDropdownOptions, serverConfig['domain_game']);
+        dropDownOption.link.addEventListener("click", () => this.activate())
+    }
+
+    activate() {
+        serverSelector.innerHTML = this.serverConfig['domain_game']
+
+        worldSelectorDropdownOptions.innerHTML = ""; // Clear previous options
+        this.worldDropDowns = []
+        for (let world of this.serverConfig.worlds) {
+            const dropDownOption = new DropDownOption(worldSelector, worldSelectorDropdownOptions, world);
+            this.worldDropDowns.push(dropDownOption);
+
+            dropDownOption.link.addEventListener("click", () => {
+                clearChart();
+                resetGraphAdder();
+
+                // Get the data - if not retrieved already
+                new Promise((resolve, reject) => {
+
+                    if (this.data) {
+                        resolve('Data loaded from previous save.');
+                    }
+                    else {
+                        axios.get(`${githubLocation}/${this.server}/${world}/tribes.json`)
+                            .then(data => {
+                                data = data.data;
+                                console.log(data);
+                                this.data = data;
+                                resolve('Data retrieved via XHR.');
+                            })
+                    }
+                }).then((message) => {
+                    //console.log(message);
+                    let tribeList = this.data.tribes.map(function (value) {
+                        return value.tribe;
+                    });
+                    //console.log(tribeList);
+
+                    graphAdder.oninput = () => {
+                        // Remove the previous tribe elements
+                        tribePopupContainer.innerHTML = "";
+
+                        if (graphAdder.value == "") return;
+
+                        let matchingTribes = tribeList.filter((x) => {
+                            return x.toLowerCase().startsWith(graphAdder.value.toLowerCase());
+                        })
+
+
+                        matchingTribes.forEach((tribe, i) => {
+                            // Find the matching tribe in all the data
+                            let matchingTribeData = this.data.tribes.filter((x) => {
+                                return x.tribe == tribe;
+                            })
+                            new TribeSelector(tribe, matchingTribeData[0]);
+                        });
+                    }
+                })
+            })
+
+
+        }
+        this.worldDropDowns[0].activate();
+
+        resetGraphAdder();
+    }
+}
+
+class DropDownOption {
+    link
+    selector
+    text
+
+    constructor(selector, parent, text = "") {
+        const newOption = document.createElement("li");
+        const newLink = document.createElement("a");
+        newLink.setAttribute("class", "dropdown-item");
+        newLink.setAttribute("href", "#");
+
+        newLink.innerHTML = text;
+
+        newLink.addEventListener("click", () => { this.activate() })
+
+        // Build the DOM
+        newOption.appendChild(newLink);
+        parent.appendChild(newOption);
+
+        // Store properties
+        this.link = newLink;
+        this.selector = selector;
+        this.text = text;
+    }
+
+    activate() {
+        this.selector.innerHTML = this.text
+
+        /* Need to make subclass of this only for the world selector that stores the server and the world, or just add the below function when the world gets created higher up */
+
+    }
+
+
+}
 
 class ServerContainer {
     worldSelectorContainer
@@ -41,10 +167,6 @@ class ServerContainer {
         this.worldButtons = []
         this.server = serverConfig['server']
 
-        const serverButton = document.createElement('button');
-        this.serverButton = serverButton;
-        serverButton.classList.add('btn', 'btn-primary');
-        serverButton.innerHTML = this.server.toUpperCase();
 
         const worldSelectorContainer = document.createElement('div');
         this.worldSelectorContainer = worldSelectorContainer;
@@ -193,6 +315,7 @@ function clearChart() {
 class TribeSelector {
     tribeName
     tribeData
+    all_data
 
     constructor(tribeName, tribeData) {
         this.tribeName = tribeName;
@@ -207,16 +330,17 @@ class TribeSelector {
     }
 
     selectTribe() {
-        console.log(this.tribeData)
-        //mainChart.setTitle({ text: this.tribeData.tribe })
+        activeTribeSelector = this;
+        this.calculateData();
+        redrawChart();
+    }
 
-        // Format the data as needed
-
+    calculateData() {
         let all_data = []
         for (let player of this.tribeData.players) {
             let player_data = []
             for (let i = 0; i < player.dates.length; i++) {
-                player_data.push([player.dates[i], player.points[i]])
+                player_data.push([player.dates[i], player[selectedMetric][i]])
             }
             all_data.push({
                 name: player.name,
@@ -224,85 +348,88 @@ class TribeSelector {
             });
         }
 
-        console.log(all_data)
-
-        /* all_data = [{
-             data: [[4, 5], [3, 6]],
-             name: "Point2",
-             color: "#00FF00"
-         }]*/
-
-        //mainChart.update({ series: all_data });
-        clearChart();
-        Highcharts.chart('plotContainer', {
-            chart: {
-                type: 'line',
-                zoomType: 'xy'
-            },
-
-            subtitle: {
-                text: 'Created by higamy'
-            },
-
-            yAxis: {
-                title: {
-                    text: 'Points'
-                }
-            },
-            title: {
-                text: this.tribeData.tribe
-            },
-
-            xAxis: {
-                type: 'datetime',
-                dateTimeLabelFormats: { // don't display the dummy year
-                    month: '%e. %b',
-                    year: '%b'
-                },
-                title: {
-                    text: 'Date'
-                }
-            },
-
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
-
-            plotOptions: {
-                series: {
-                    marker: {
-                        enabled: true
-                    }
-                }
-            },
-
-            series: all_data,
-
-
-            responsive: {
-                rules: [{
-                    condition: {
-                        //  maxWidth: 500
-                    },
-                    chartOptions: {
-                        plotOptions: {
-                            series: {
-                                marker: {
-                                    radius: 2.5
-                                }
-                            }
-                        }
-                    }
-                }]
-            }
-
-        });
-
-        //mainChart.series[0].setData(all_data);
-        //mainChart.redraw()
+        this.all_data = all_data
     }
 
 }
 
+function redrawChart() {
+    activeTribeSelector.calculateData();
+    clearChart();
+
+    Highcharts.chart('plotContainer', {
+        chart: {
+            type: 'line',
+            zoomType: 'xy'
+        },
+
+        subtitle: {
+            text: 'Created by higamy'
+        },
+
+        yAxis: {
+            title: {
+                text: selectedMetric
+            }
+        },
+        title: {
+            text: `${activeTribeSelector.tribeData.tribe} (${selectedMetric})`
+        },
+
+        xAxis: {
+            type: 'datetime',
+            dateTimeLabelFormats: { // don't display the dummy year
+                month: '%e. %b',
+                year: '%b'
+            },
+            title: {
+                text: 'Date'
+            }
+        },
+
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        plotOptions: {
+            series: {
+                marker: {
+                    enabled: true
+                }
+            }
+        },
+
+        series: activeTribeSelector.all_data,
+
+
+        responsive: {
+            rules: [{
+                condition: {
+                    //  maxWidth: 500
+                },
+                chartOptions: {
+                    plotOptions: {
+                        series: {
+                            marker: {
+                                radius: 2.5
+                            }
+                        }
+                    }
+                }
+            }]
+        }
+
+    });
+}
+
+// Set up the drop down to select the statistic
+for (let statistic of availableStatistics) {
+    const dropDownOption = new DropDownOption(statisticSelector, statisticsDropdownOptions, statistic);
+
+    dropDownOption.link.addEventListener("click", () => {
+        selectedMetric = statistic;
+        redrawChart();
+    })
+}
