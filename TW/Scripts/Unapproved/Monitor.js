e26917c3db3a9627ae9cfe45e954d87f8eecea99
Will Higham <wshigham@gmail.com>
2023-11-14 17:12:35 +0000

    Monitor changed to use service worker

--- TW/Scripts/Unapproved/Monitor.js
+++ TW/Scripts/Unapproved/Monitor.js
@@ -1,30 +1,57 @@
+let worker = new Worker(
+    `data:text/javascript,
+    onmessage = function(event){    //This will be called when worker.postMessage is called in the outside code.
+        let foo = event.data;    //Get the argument that was passed from the outside code, in this case foo.
+        console.log(foo)
+        const url = 'https://higamy.com/send_message';
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(foo),
+        };
+        
+        fetch(url, options)
+            .then(response => postMessage(response['statusText']) )
+            .catch(error => {
+            console.error('Error:', error);
+            });
+
+          //Send the result to the outside code.
+    };
+    `
+);
+
+worker.onmessage = function (event) {    //Get the result from the worker. This code will be called when postMessage is called in the worker.
+    console.log("The result is " + event.data);
+    UI.SuccessMessage(`Server response: ${event.data}`)
+
+    // Set the next iteration to be queued
+    setTimeout(sendPostRequest, 15000);
+}
+
 function sendPostRequest() {
+
     let woodAmount = parseInt(document.querySelector("#premium_exchange_rate_wood > div:nth-child(1)").textContent);
     let clayAmount = parseInt(document.querySelector("#premium_exchange_rate_stone > div:nth-child(1)").textContent);
     let ironAmount = parseInt(document.querySelector("#premium_exchange_rate_iron > div:nth-child(1)").textContent);
 
-    console.log(woodAmount, clayAmount, ironAmount)
+    let data = {
+        server: game_data.market,
+        world: game_data.world,
+        continent: game_data.village.display_name.slice(-3,),
+        wood: woodAmount,
+        clay: clayAmount,
+        iron: ironAmount
+    }
 
-    jQuery.ajax({
-        url: 'https://higamy.com/send_message',
-        method: 'POST',
-        data: {
-            server: game_data.market,
-            world: game_data.world,
-            continent: game_data.village.display_name.slice(-3,),
-            wood: woodAmount,
-            clay: clayAmount,
-            iron: ironAmount
-        },
-        dataType: 'JSON',
-        success: function ({ message }) {
-            UI.SuccessMessage(message);
-        },
-    });
-
+    worker.postMessage(data);
 }
 
+setTimeout(sendPostRequest, 1000);
+
 // Call the function when the page loads (for testing purposes)
-setInterval(sendPostRequest, 15000);
+//setInterval(sendPostRequest, 15000);
 UI.SuccessMessage('Started monitoring!')
 
