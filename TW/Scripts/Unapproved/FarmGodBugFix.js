ecc353c2d379d28b75cd429564ff4e9612a2c08a
Will Higham <wshigham@gmail.com>
2024-04-11 11:57:44 +0100

    Tidied up bug fix code.

--- TW/Scripts/Unapproved/FarmGodBugFix.js
+++ TW/Scripts/Unapproved/FarmGodBugFix.js
@@ -1,5 +1,5 @@
 // Hungarian translation provided by =Krumpli=
-let debugVar = [];
+
 
 ScriptAPI.register('FarmGod', true, 'Warre', 'nl.tribalwars@coma.innogames.de');
 
@@ -116,7 +116,6 @@ window.FarmGod.Library = (function () {
         twLib.init();
     }
 
-
     /**** Script Library ****/
     const setUnitSpeeds = function () {
         let unitSpeeds = {};
@@ -139,6 +138,9 @@ window.FarmGod.Library = (function () {
     const determineNextPage = function (page, $html) {
         let villageLength = ($html.find('#scavenge_mass_screen').length > 0) ? $html.find('tr[id*="scavenge_village"]').length : $html.find('tr.row_a, tr.row_ax, tr.row_b, tr.row_bx').length;
         let navSelect = $html.find('.paged-nav-item').first().closest('td').find('select').first();
+        // Commented out the old version of the code, updated in April 2024
+        // The old version did not count the number of pages in the loot assistant properly when there were more than 15 or so due to the way the UI changes to not show all pages
+        // let navLength = ($html.find('#am_widget_Farm').length > 0) ? $html.find('#plunder_list_nav').first().find('a.paged-nav-item').length : ((navSelect.length > 0) ? navSelect.find('option').length - 1 : $html.find('.paged-nav-item').not('[href*="page=-1"]').length);
         let navLength = ($html.find('#am_widget_Farm').length > 0) ? parseInt($('#plunder_list_nav').first().find('a.paged-nav-item')[$('#plunder_list_nav').first().find('a.paged-nav-item').length - 1].text.replace(/\D/g, '')) - 1 : ((navSelect.length > 0) ? navSelect.find('option').length - 1 : $html.find('.paged-nav-item').not('[href*="page=-1"]').length);
         let pageSize = ($('#mobileHeader').length > 0) ? 10 : parseInt($html.find('input[name="page_size"]').val());
 
@@ -165,7 +167,6 @@ window.FarmGod.Library = (function () {
         let page = (url.match('am_farm') || url.match('scavenge_mass')) ? 0 : -1;
         let wrapFn = function (page, $html) {
             let dnp = determineNextPage(page, $html);
-            console.log(dnp, page, $html)
 
             if (dnp) {
                 processorFn($html);
